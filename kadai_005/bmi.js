ab08596304296c093f589cc312a7c7d9f7307576
tatsuya8821 <134764703+tatsuya8821@users.noreply.github.com>
2023-07-16 14:17:48 +0900

    kadai_005四則演算　再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -11,11 +11,16 @@ let userheight;
 userheight=1.7;
 
 
-//コンソールへの出力　体重・身長
+//変数の宣言
+let userBmi;
+
+//値の代入・BMI
+userBmi=68/(1.7*1.7);
+
+//コンソールへの出力　体重・身長・BMI
 console.log(userWeight);
 console.log(userheight);
-console.log(68/(1.7*1.7));
-
+console.log(userBmi);
 
 
 
