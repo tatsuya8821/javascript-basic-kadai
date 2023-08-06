b47fc94c0645540d0f7f94641704ad712ff3623e
tatsuya8821 <134764703+tatsuya8821@users.noreply.github.com>
2023-08-06 16:07:02 +0900

    DOM操作を使用して文字表示を変えようのファイルをアップ

---
+++ kadai_019/event.js
@@ -0,0 +1,9 @@
+// 新しくbutton要素を作成し、定数に代入する
+const btn = document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+ 
+ //指定した要素に「ボタンがクリックされました」というテキストを追加
+ text.textContent = 'ボタンがクリックされました';
+});
